a0f2d9957545f17b2154dd153609eff116dc76c0
andrei22george <andrei22george@gmail.com>
2026-01-14 18:59:45 +0100

    Update model hyperparams

--- data/training/ppl-train.py
+++ data/training/ppl-train.py
@@ -19,9 +19,9 @@ for env in required_env:
 # Hyperparameters (must be explicitly defined and logged)
 MODEL_NAME = "distilbert-base-uncased"
 LEARNING_RATE = 2e-5
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 EPOCHS = 4
-MAX_LENGTH = 512
+MAX_LENGTH = 256
 LIMIT = "" 
 
 accuracy_metric = torchmetrics.Accuracy(task='binary')
